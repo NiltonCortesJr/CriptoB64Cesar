bc619692aa5b61961e2d4edc64b791a42ea8f5f5
NiltonCortesJr <niltoncjr@gmail.com>
2022-01-23 17:31:20 -0300

    Início do Projeto

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//armezanando o texto digitado pelo usuário
+// var digitado = prompt("Digite o texto a ser Codificado / Decodificado: ")
+
+
+// Codificando em Base 64
+
+var emBase64 = btoa(digitado);
+console.log(emBase64);
+
+
+// Decodificando para string
+
+var deBase64 = atob(digitado);
+console.log(deBase64);
+
+
+
+
