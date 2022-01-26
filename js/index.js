dd8decea291edb0903161dabe4f8bdd3dbd54d1b
NiltonCortesJr <niltoncjr@gmail.com>
2022-01-26 05:16:10 -0300

    Projeto atualizado - ainda não concluído.

--- js/index.js
+++ js/index.js
@@ -1,49 +1,164 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-//armezanando o texto digitado pelo usuário
-// var digitado = prompt("Digite o texto a ser Codificado / Decodificado: ")
-
-// declarando as variáveis
-// var fraseDigitada = document.getElementById("entrada");
-
-// var codifica = document.getElementById("codifica");
-
-// var decodifica = document.getElementById("decodifica");
-
-var metodo = document.getElementById("metodoEscolhido");
-
-// var metodoCesar = document.getElementById("avanco");
-
-// var botao = document.getElementById("rodarBotao");
-
-// var resultado = document.getElementById("digitavel")
-// var resposta = ""
+// Armezanando o texto digitado pelo usuário
+   var fraseDigitada = document.getElementById("entrada");
 
 
-const rango = document.getElementById('rango');
-// testando avanço - rango
-console.log(parseInt(rango.value));
+// Declarando variáveis
+   var metodoCesar = document.getElementById("avanco");
+   var metodo = document.getElementById("metodoEscolhido")
 
 
-
-// Codificando em Base 64
-
-var emBase64 = btoa(digitado);
-console.log(emBase64);
-
-
-// Decodificando para string
-
-var deBase64 = atob(digitado);
-console.log(deBase64);
-
-metodo.addEventListener("change", function(){
-    if (metodo.value == "1"){
-       document.getElementById("seCesar").style.display = "block"
-    }
-    else{
-       document.getElementById("seCesar").style.display = "none"
-    }
- } )
+// variáveis do botao
+   var codifica = document.getElementById("codificaMsg");
+   var decodifica = document.getElementById("decodificaMsg");
+   var botao = document.getElementById("rodaBotao");
 
 
+// variáveis de Retorno da codificação / decodificação
+   var resultado = document.getElementById("saida");
+   var mensagemRetornada = "";
+
+
+// Mudar a mensagem do botao
+   codifica.addEventListener("click", function () {
+      botao.textContent = "C o d i f i c a r"
+   });
+
+   decodifica.addEventListener("click", function () {
+      botao.textContent = "D e c o d i f i c a r"
+   });
+
+
+// Ocultando o avanço se Base 64
+   function funcaoSelect(valor) {
+      
+      if(valor == "2"){
+         document.getElementById("casoCesar").style.display = "none";
+      } else if(valor == "1"){
+         document.getElementById("casoCesar").style.display = "flex";
+         document.getElementById("casoCesar").style.flexDirection = "column";
+      }
+   }
+
+
+//Validando o checkbox Codificar/Decodificar
+
+   if(metodo.value == 1){
+      if(codifica.checked ){
+         mensagemRetornada = cifraDeCesar(fraseDigitada.value, metodoCesar.value)
+         resultado.setAttribute("value",mensagemRetornada)
+      }
+      else{
+         mensagemRetornada = decodicaCesar(fraseDigitada.value, metodoCesar.value)
+         resultado.setAttribute("value",mensagemRetornada)
+      }
+   }   
+   
+
+   // Cofificando se Cifra de Cesar
+
+   function cifraDeCesar (texto, adiciona){
+
+      var adiciona = parseInt(adiciona)
+      var armazenaMensagem = texto.toLowerCase()
+      armazenaMensagem = armazenaMensagem.split("")
+      var mensagemCriptografada = []
+      var cesar = []
+
+      //Percorrendo o tamanho da mensagem
+
+      for (var i=0; i<armazenaMensagem.length; i++){
+         var msg = armazenaMensagem[i].charCodeAt()
+         if (msg != 32){ 
+            mensagemCriptografada.push(((msg - 97 + adiciona) % 26) + 97)
+         }
+         else {
+            mensagemCriptografada.push(msg)
+         }
+      }
+      for (var j=0; j<mensagemCriptografada.length; j++){
+         cesar.push(String.fromCharCode(mensagemCriptografada[j]))
+      }
+      return cesar.join("")
+   }
+
+
+   // Decofificando se Cifra de Cesar
+
+   function decodicaCesar (texto, adiciona){
+
+      var adiciona = parseInt(adiciona)
+      var armazenaMensagem = texto.split("")
+      armazenaMensagem = armazenaMensagem.toLowerCase()
+      var mensagemCriptografada = []
+      var cesar = []
+
+      //Percorrendo o tamanho da mensagem
+
+      for (var i=0; i<armazenaMensagem.length; i++){
+         var msg = armazenaMensagem[i].charCodeAt()
+         if (msg != 32){
+            if ((msg - 97 + adiciona) >=0 ){
+               mensagemCriptografada.push(((msg - 97 - adiciona) % 26) + 97)
+            }
+            else{
+               mensagemCriptografada.push(((msg - 97 - adiciona) + 26) + 97)
+            }
+         }
+         else {
+            mensagemCriptografada.push(msg)
+         }
+      }
+      for (var j=0; j<mensagemCriptografada.length; j++){
+         cesar.push(String.fromCharCode(mensagemCriptografada[j]))
+      }
+      return cesar.join("")
+   }
+   
+// Validando checkbox para Codificar / Decodificar
+
+   // Se método Cifra de Cesar
+
+   // Codificando
+
+   if(metodo.value == 1){
+      if(codifica.checked ){
+         mensagemRetornada = cifraDeCesar(fraseDigitada.value, metodoCesar.value)
+         resultado.setAttribute("value",mensagemRetornada)
+      }
+      
+      // Decodificando
+
+      else{
+         mensagemRetornada = decodicaCesar(fraseDigitada.value, metodoCesar.value)
+         resultado.setAttribute("value",mensagemRetornada)
+      }
+   }
+
+
+   // Se método Base 64
+
+      // Codificando Base 64
+
+      else if(metodo.value == 2){
+         if(codifica.checked ){
+            //console.log("ola")
+            mensagemRetornada = window.btoa(fraseDigitada.value)
+            resultado.setAttribute("value",mensagemRetornada)
+         } 
+
+      // Decodificando Base 64 
+      
+      else {
+         mensagemRetornada = window.atob(fraseDigitada.value)
+         resultado.setAttribute("value",mensagemRetornada)
+      }
+   }
+
+
+// Clicando no botão
+
+   document.getElementById("rodaBotao").addEventListener("onclick", function(event){
+      event.preventDefault();
+   });
+
+// Fim do Código JavaScript
